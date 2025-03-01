cbfca04620cc43024a02843281247de85736f25d
krishnaaa210 <krishnaaa210@gmail.com>
2025-03-01 21:31:40 +0530

    AddedModel

---
+++ ePizza.Models/Response/ApiResponseModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ePizza.Models.Responses
+{
+    public class ApiResponseModel<T>
+    {
+        public bool Success { get; set; }
+
+        public T Data { get; set; }
+
+        public string Message { get; set; }
+
+        public ApiResponseModel(bool success,T data,string message)
+        {
+            Success = success;
+            Data = data;
+            Message = message;
+        }
+    }
+}
