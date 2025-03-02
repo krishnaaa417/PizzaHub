242e9d9c7879eda518387fa6bb97aa890caf9b10
krishnaaa210 <krishnaaa210@gmail.com>
2025-03-02 19:36:17 +0530

    Added responses

---
+++ ePizza.Models/Response/AuthApiResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ePizza.Models.Responses
+{
+    public class AuthApiResponse
+    {
+        public string AccessToken {  get; set; }
+    }
+}
